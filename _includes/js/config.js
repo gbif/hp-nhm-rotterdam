b5416a4472145ad91bbdc77c297d11fdf2fde2d2
langeveldNMR <69208596+langeveldNMR@users.noreply.github.com>
2021-03-09 12:40:00 +0100

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'collectionID', value: '187a8a55-7de6-4230-8250-7d4ee274004f' }
+  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }
 };
