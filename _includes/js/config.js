52f030940c7ac0e165c79c754816ff5a091c3600
langeveldNMR <69208596+langeveldNMR@users.noreply.github.com>
2021-04-14 14:53:10 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,5 +5,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 var siteConfig = {
   rootPredicate: { type: 'equals', key: 'datasetKey', value: 'a307e4d7-1de2-4adc-95d5-a0a8d5f57236' },
   blacklistedFilters: ['datasetKey'], // no need to have a dataset filter when we only have one dataset
-  highlightedFilters: ['q', 'taxonKey', 'catalogNumber', 'locality', 'countryCode'], // or whatever you believe is the most useful filters for your audience
+  highlightedFilters: ['q', 'taxonKey', 'catalogNumber', 'locality', 'waterBody', 'year', 'countryCode'], // or whatever you believe is the most useful filters for your audience
 };
