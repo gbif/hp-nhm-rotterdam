1c119a6d51c654d302c2dcb037f48fe326f11496
langeveldNMR <69208596+langeveldNMR@users.noreply.github.com>
2021-04-14 14:25:33 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,4 +5,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 var siteConfig = {
   rootPredicate: { type: 'equals', key: 'datasetKey', value: 'a307e4d7-1de2-4adc-95d5-a0a8d5f57236' },
   blacklistedFilters: ['datasetKey'], // no need to have a dataset filter when we only have one dataset
+  highlightedFilters: ['taxonKey', 'catalogNumber', 'recordedBy', 'recordNumber', 'typeStatus'], // or whatever you believe is the most useful filters for your audience
 };
