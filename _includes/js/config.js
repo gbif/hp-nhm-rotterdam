58bdfe3e8cd085a508799170ea139031343d763c
Morten Høfft <mhoefft@gbif.org>
2021-05-07 09:38:24 +0200

    Update terminology for filter exclusion

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -4,6 +4,6 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 
 var siteConfig = {
   rootPredicate: { type: 'equals', key: 'datasetKey', value: 'a307e4d7-1de2-4adc-95d5-a0a8d5f57236' },
-  blacklistedFilters: ['datasetKey'], // no need to have a dataset filter when we only have one dataset
+  excludedFilters: ['datasetKey'], // no need to have a dataset filter when we only have one dataset
   highlightedFilters: ['q', 'taxonKey', 'catalogNumber', 'locality', 'waterBody', 'year', 'countryCode', 'basisOfRecord'], // or whatever you believe is the most useful filters for your audience
 };
