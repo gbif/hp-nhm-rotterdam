2c33ffc3142e7c70e24a7d5b9320c88daa5db94b
langeveldNMR <69208596+langeveldNMR@users.noreply.github.com>
2021-12-13 08:36:03 +0100

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,5 +6,5 @@ var siteConfig = {
   rootPredicate: { type: 'equals', key: 'datasetKey', value: 'a307e4d7-1de2-4adc-95d5-a0a8d5f57236' },
   excludedFilters: ['datasetKey'], // no need to have a dataset filter when we only have one dataset
   highlightedFilters: ['q', 'taxonKey', 'catalogNumber', 'locality', 'waterBody', 'year', 'country', 'basisOfRecord'], // or whatever you believe is the most useful filters for your audience
-  defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'catalogNumber', 'basisOfRecord'],
+  defaultTableColumns: ['features', 'preparations', 'catalogNumber', 'country', 'coordinates', 'year', 'basisOfRecord'],
 };
